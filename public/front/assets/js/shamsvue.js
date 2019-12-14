1cb00fbb107722ac3c8a28c34d96fcc9c7566df8
MD ALI AKBAR TIPU <aliakbartipu4@gmail.com>
2019-12-14 11:10:15 +0600

    HomePage YOUR COLLECTION product block placed with sliding

--- public/front/assets/js/shamsvue.js
+++ public/front/assets/js/shamsvue.js
@@ -439,54 +439,63 @@ Vue.component('product_article', {
             link: "/singleProduct/1" + this.id,
         }
     },
-    props: ['thumb1', 'thumb2', 'name', 'price', 'id'],
+    props:['thumb1', 'thumb2'],
     template: `
-    <article class="single_product">
+    <div class="slide">
+    <article class="single_product mt-3">
         <figure>
-            <div class="product_thumb">
-                <a class="primary_img" :href="link"><img :src="thumb1" alt=""></a>
-                <a class="secondary_img" :href="link"><img :src="thumb2" alt=""></a>
+
+            <div class="product_thumb last-day-11">
+
+
+                <a class="primary_img" href="product-countdown.html"><img :src="thumb1" alt=""></a>
+                <a class="secondary_img" href="product-countdown.html"><img :src="thumb2" alt=""></a>
+
                 <div class="label_product_left label_product">
                     <span class="label_sale_left">New</span>
                 </div>
-                    <div class="label_product">
-                    <span class="label_sale">29%</span>
+                <div class="label_product">
+                    <span class="label_sale">Sale</span>
                 </div>
                 <div class="action_links">
                     <ul>
-                        <li class="wishlist"><a href="#" title="Add to Wishlist"><i class="ion-android-favorite-outline"></i></a></li>
+                        <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i class="ion-android-favorite-outline"></i></a></li>
                         <li class="compare"><a href="#" title="Add to Compare"><i class="ion-ios-settings-strong"></i></a></li>
-                        <li class="quick_button"><a href="#" data-toggle="modal" data-target="#modal_box"  title="10 colors | quick view"><i class="ion-ios-search-strong"></i></a></li>
+                        <li class="quick_button"><a href="#" data-toggle="modal" data-target="#modal_box" title="10 colors | quick view" class="inner-search-back" ><i class="ion-ios-search-strong"></i></a></li>
                     </ul>
                 </div>
             </div>
             <div class="product_content">
-                <div class="product_timing">
-                        <div data-countdown="2020/02/16"></div>
-                    </div>
+                <div class="product_timing sinhle-count">
+                    <div data-countdown="2021/02/15"></div>
+                </div>
                 <div class="product_content_inner">
-                    <h2 class="product_name_brand_name">{{name}}</h2>
-                    <h3 class="product_name"><a href="product-countdown.html">{{name}}</a></h3>
-                    <h4 class="product_name_h4"><a href="">New ArriVal</a></h4>
+                    <h2 class="product_name_brand_name mt-1">G-Star Raw</h2><br>
+                    <h3 class="product_name"><a href="product-countdown.html">Women's Designer Top</a></h3>
+                    <h4 class="product_name_h4"><a href="">New Markdown</a></h4>
                     <div class="price_box">
-                        <span class="old_price">Reg. $49</span><br />
-                        <span class="current_price">Sale {{price}}</span>
+                        <span class="old_price">Reg. $599.99</span><br />
+                        <span class="current_price">Sale $599.99</span>
                     </div>
                     <div class="countdown_text mb-3">
-                        <!-- <a href="">Extra 15% off use:SUNDAY </a>-->
-                        <a href="" class="chng-color">Free shipping at $45</a>
+                        <a href="">Extra 15% off use:SUNDAY </a>
+                        <a href="" class="chng-color">Free shipping at $175</a>
                     </div>
                     <div class="star_icon">
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+
                     </div>
                 </div>
+
+
             </div>
         </figure>
     </article>
+</div>
     `
 });
 
@@ -494,15 +503,28 @@ Vue.component('product_article', {
 
 
 Vue.component('tab_products', {
-
+    data: function(){
+        return {
+            product:{
+                images:{
+                    thumb1:"/front/assets/img/product/thumb1.jpg",
+                    thumb2:"/front/assets/img/product/thumb2.jpg",
+                    all:""
+                }
+            },
+            count:10,
+        }
+    },
     template: `
-    <div class="tab-pane fade show active" id="Fashion" role="tabpanel">
-        <div class="product_carousel product_style product_column5 owl-carousel">
-
-           <slot></slot>
-                            
+    <div class="tab-pane fade show" id="deals" role="tabpanel">
+        <section class="customer-logos">
+                <product_article v-for="c in count" v-bind:key="c.text"
+                    :thumb1="product.images.thumb1"
+                    :thumb2="product.images.thumb2"
+                ></product_article>    
+                
+           </section>   
         </div>
-    </div>
     `
 });
 
--- resources/views/front/section/homeSection.php
+++ resources/views/front/section/homeSection.php
@@ -1,38 +1,35 @@
 <div class="clearfix"></div>
-<div>
-</div>
 
 
 <div class="home_section_bg">
-        <div class="product_area deals_product">
-            <div class="container">
+    <div class="product_area deals_product">
+        <div class="container">
             <div class="para-six">
                 <div class="row">
                     <div class="col-12 col-md-9 offset-md-3">
                         <div class="product_header">
                             <div class="product_tab_btn text-center">
-							<h3 class="mb-2">YOUR COLLECTION</h3>
-                             <p class="text-center time-to">Trending Fashion</p>
+                                <h3 class="mb-2">YOUR COLLECTION</h3>
+                                <p class="text-center time-to">Trending Fashion</p>
                                 <ul class="nav text-center" role="tablist">
                                     <li>
                                         <a class="active" data-toggle="tab" href="#new" role="tab" aria-controls="Fashion" aria-selected="true">
-                                           New Arrivals
+                                            New Arrivals
                                         </a>
                                     </li>
                                     <li>
                                         <a data-toggle="tab" href="#deals" role="tab" aria-controls="Games" aria-selected="false">
-                                        Deals Of The Month
+                                            Deals Of The Month
                                         </a>
                                     </li>
                                     <li>
                                         <a data-toggle="tab" href="#featured" role="tab" aria-controls="Speaker" aria-selected="false">
-                                           Featured Products
+                                            Featured Products
                                         </a>
                                     </li>
                                     <li>
                                         <a data-toggle="tab" href="#best" role="tab" aria-controls="Mobile" aria-selected="false">
-                                           
-										Best Selling Products
+                                            Best Selling Products
                                         </a>
                                     </li>
                                 </ul>
@@ -40,95 +37,21 @@
                         </div>
                     </div>
                 </div>
-                <div class="tab-content">
-                    <div class="tab-pane fade show active" id="Fashion" role="tabpanel">
-                        <div class="product_carousel product_style product_column5 owl-carousel">
-                        <?php 
-                        foreach($products as $product): ?>
-                        <product_article 
-                            thumb1="/front/assets/img/product/thumb1.jpg"
-                            thumb2="/front/assets/img/product/thumb2.jpg"
-                            name="<?=$product->name?>"
-                            price="<?=$product->price?>"
-                            id="<?=$product->id?>"
-                        ></product_article>
-                        <?php endforeach; ?>                            
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="deals" role="tabpane2">
-                        <div class="product_carousel product_style product_column5 owl-carousel">
-                        
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="best" role="tabpane3">
-                        <div class="product_carousel product_style product_column5 owl-carousel">
-                        
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="featured" role="tabpane3">
-                        <div class="product_carousel product_style product_column5 owl-carousel">
-                        
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
 
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
 
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                            <product_article 
-                                thumb1="/front/assets/img/product/thumb1.jpg"
-                                thumb2="/front/assets/img/product/thumb2.jpg"
-                            ></product_article>
-                        </div>
-                    </div>
+                <div class="tabcontent">
+                            <tab_products>
+                            </tab_products>           
                 </div>
+            
+            <!-- tab content end here -->
+
+        </div>
+    </div>
+</div>
+<!--product area end-->
+
+<!--banner area start-->
+
 
 </div>
-            </div>
-        </div>
-        <!--product area end-->
-
-        <!--banner area start-->
-
-
-    </div>
-   
