13cf2dff546585f9f2a6884552cd7d3ba99e3b83
Shams Pathan <user.beshi@gmail.com>
2019-12-20 23:20:21 +0600

    attributes

--- database/migrations/2019_12_20_231137_create_attributes_table.php
+++ database/migrations/2019_12_20_231137_create_attributes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBrandTable extends Migration
+class CreateAttributesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateBrandTable extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('attributes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('image');
-            $table->boolean('active');
+            $table->unsignedInteger('fits');
+            $table->unsignedInteger('sleeves');
+            $table->unsignedInteger('leg_lengths');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateBrandTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('attributes');
     }
 }
