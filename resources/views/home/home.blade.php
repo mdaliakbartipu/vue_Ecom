f7f312301f9caf223ded41c2f44caebb522aee7d
MD ALI AKBAR TIPU <aliakbartipu4@gmail.com>
2020-01-18 13:46:10 +0600

    Chart is taking place

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,15 +4,39 @@
     <i class="fa fa-tachometer"></i> Admin Panel
 @stop
 @section('css')
-    
+    <style>
+        #chartContainer{
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
+        #chartContainer div{
+            width: 24%;
+        }
+    </style>
 @stop
 
 
 @section('content')
 
-<div class="container" style="margin-top:10em">
-        <span class=" label label-info arrowed-right arrowed-in" style="height:100px;width:90%;font-size:5em">D7Brigus Admin Panel</span>
-</div>
+<div id="chartContainer" class="container" style="margin-top:1em;">
+        <!-- <span class=" label label-info arrowed-right arrowed-in" style="height:100px;width:90%;font-size:5em">D7Brigus Admin Panel</span> -->
+
+        <div style="height:250px;">
+            <canvas id="myChart" width="100" height="100"></canvas>
+        </div>
+        <div style="height:250px;background:rebeccapurple">
+            <!-- <canvas id="myChart" width="100" height="100"></canvas> -->
+        </div>
+        <div style="height:250px;background:rebeccapurple">
+            <!-- <canvas id="myChart" width="100" height="100"></canvas> -->
+        </div>
+        <div style="height:250px;background:rebeccapurple">
+            <!-- <canvas id="myChart" width="100" height="100"></canvas> -->
+        </div>
+        
+
+    </div>
 
 @endsection
 
@@ -28,6 +52,47 @@
 
     <script src="{{ asset('assets/js/ace-elements.min.js') }}"></script>
     <script src="{{ asset('assets/js/ace.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <!-- Chart JS -->
+    <script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
 
     <script type="text/javascript">
         jQuery(function($) {
